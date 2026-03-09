432777a7509af9c09c5b7475e76242b0facce03b
oanaavasile <oana.97v@gmail.com>
2026-03-09 21:15:12 +0200

    added new file

---
+++ tests/test.spec.js
@@ -0,0 +1,9 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('login', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
+
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Playwright/);
+});
