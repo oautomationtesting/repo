8217307e54eb8df43601dba1801209d3080ef708
oanaavasile <oana.97v@gmail.com>
2026-03-10 14:51:54 +0200

    login tests

---
+++ tests/test-login.spec.js
@@ -0,0 +1,7 @@
+test('login', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
+
+    // Click the get started link.
+    await page.getByRole('link', { name: 'Get started' }).click();
+
+});
