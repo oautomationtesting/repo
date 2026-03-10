786e27e5610112a5a6e08001876f9ef6d367541f
oanaavasile <oana.97v@gmail.com>
2026-03-10 16:04:21 +0200

    mst ch

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -8,11 +8,11 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
+test('master title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole('link', { name: 'master' }).click();
   await page.getByRole('link', { name: 'Get started' }).click();
 
   await page.goto("myurl");;
